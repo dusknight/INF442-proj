6303a5075d6b7ae92b90104c5ce1c5a639b9e09a
Shikang DU <skdu.dusk@gmail.com>
2020-05-01 12:15:52 +0200

    fix bugs

---
+++ cmake-build-debug/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/Graph.cpp.o"
+  "libtest.dll.a"
+  "test.exe"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
